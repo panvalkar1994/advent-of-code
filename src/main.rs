1c9a083c70c6295a58d7b89cfea235cd2d140c2b
Vaibhav Panvalkar <vaibhav.panvalkar@elastic.run>
2023-02-08 23:05:46 +0530

    2015:day2:part1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::fs;
 
-use advent_of_code::aoc15::day1;
+use advent_of_code::aoc15::day2;
 
 fn main() {
     let args:Vec<String> = std::env::args().collect();
     let file = args.get(1).unwrap();
     let input = fs::read_to_string(file).unwrap();
 
-    let solution = day1::get_basement(input);
+    let solution = day2::get_total_sq_paper(input);
     println!("Solution: {solution}");
 }
