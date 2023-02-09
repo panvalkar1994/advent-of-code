d45f719dd7e80110c2ca6c375ecd80035379a48a
Vaibhav Panvalkar <vaibhav.panvalkar@elastic.run>
2023-02-09 21:11:00 +0530

    2015:day3:part1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::fs;
 
-use advent_of_code::aoc15::day2;
+use advent_of_code::aoc15::day3;
 
 fn main() {
     let args:Vec<String> = std::env::args().collect();
     let file = args.get(1).unwrap();
     let input = fs::read_to_string(file).unwrap();
 
-    let solution = day2::get_ribbon_length(input);
+    let solution = day3::get_count_atleast_once_visited_home(input);
     println!("Solution: {solution}");
 }
