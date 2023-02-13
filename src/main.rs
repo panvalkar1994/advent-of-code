5f54e1c57c8860b166cc7965847a37dfa5e11671
Vaibhav Panvalkar <vaibhav.panvalkar@elastic.run>
2023-02-13 09:17:22 +0530

    2015:day3:part2

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,6 @@ fn main() {
     let file = args.get(1).unwrap();
     let input = fs::read_to_string(file).unwrap();
 
-    let solution = day3::get_count_atleast_once_visited_home(input);
+    let solution = day3::get_count_atleast_once_visited_home_with_robo(input);
     println!("Solution: {solution}");
 }
